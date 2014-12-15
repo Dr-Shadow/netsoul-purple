ab466349bedcf92f5a23e3fb490f1f55bc5e67b8
kerdil_r <robin.kerdiles@epitech.eu>
2014-12-15 14:10:04 +0100

    Revert "create temporary files required by autogen"
    This reverts commit 220ee26fc4dce2db29f4b0c959ce33de57a21b3d.

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,6 @@
 echo "Generating configuration files for Netsoul_Pidgin, please wait..."
 echo;
 
-touch -f NEWS
-cp -f LICENSE COPYING
-
 echo "Running libtoolize, please ignore non-fatal messages..."
 echo n | libtoolize --copy --force || exit;
 echo;
@@ -14,8 +11,5 @@ autoheader || exit;
 automake --add-missing --copy
 autoconf || exit;
 automake || exit;
-
-rm -f NEWS
-rm -f COPYING
 
 echo "Done."
